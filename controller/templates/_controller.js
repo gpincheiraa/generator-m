8f23fa27143b571f0ed8cf5a85518aaf104895a9
Gonzalo Pincheira Arancibia <g.pincheira.a@gmail.com>
2015-06-24 23:52:04 -0400

    modify controller template

--- controller/templates/_controller.js
+++ controller/templates/_controller.js
@@ -1,15 +1,21 @@
-'use strict';
-angular.module('<%= moduleName %>')
-.controller('<%= controllerName %>', function (<% if(options.sample === 'start') { %><%= serviceName %>, <%= configName %><% } %>) {
-
-<% if (options.sample === 'start') {-%>
-  // bind data from service
-  this.someData = <%= serviceName %>.someData;
-  this.ENV = <%= configName %>.ENV;
-  this.BUILD = <%= configName %>.BUILD;
-
-<% } -%>
-  console.log('Hello from your Controller: <%= controllerName %> in module <%= moduleName%>:. This is your controller:', this);
-  // TODO: do your controller thing
-
-});
+(function() {
+  'use strict';
+  
+  angular
+    .module('<%= moduleName %>')
+    .controller('<%= controllerName %>', '<%= controllerName %>');
+  
+  <%= controllerName %>.$inject = ['dependencies'];
+  
+  /* @ngInject */
+  function <%= controllerName %>(dependencies) {
+    
+    var vm = this;
+    vm.title = 'Controller';
+    activate();
+    
+    ///////FUNCIONES PRIVADAS/////////
+    function activate() {
+    }
+  }
+})();
